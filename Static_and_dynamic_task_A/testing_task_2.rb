3b98dd6f144784e089d809900a2ecbc4af49dbe0
ramosjm <jjramos_2004@hotmail.com>
2019-05-02 10:36:44 +0100

    updated static and dynamic task 2.

--- Static_and_dynamic_task_A/testing_task_2.rb
+++ Static_and_dynamic_task_A/testing_task_2.rb
@@ -5,29 +5,28 @@
 
 require_relative('card.rb')
 class CardGame
-  
 
-  def checkforAce(card)
-    if card.value = 1
+  def check_for_ace(card)
+    if card.value == 1
       return true
     else
       return false
     end
   end
 
-  dif highest_card(card1 card2)
+  def highest_card(card1,card2)
   if card1.value > card2.value
-    return card.name
+    return card1
   else
-    card2
+    return card2
   end
-end
 end
 
-def self.cards_total(cards)
-  total
+def cards_total(cards)
+  total = 0
   for card in cards
     total += card.value
-    return "You have a total of" + total
   end
+    return "You have a total of" + total
+end
 end
